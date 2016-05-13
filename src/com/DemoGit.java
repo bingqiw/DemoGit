877389bd356e533e991ff187ad58c902c2615f98
bingqiw <wangbq5@asiainfo.com>
2016-05-13 15:12:27 +0800

    222

--- src/com/DemoGit.java
+++ src/com/DemoGit.java
@@ -4,7 +4,8 @@ public class DemoGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello113");System.out.println("Hello112");
+		System.out.println("Hello113");
+		System.out.println("Hello112");
 	}
 
 }
